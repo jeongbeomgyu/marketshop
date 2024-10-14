b47428cae7f2edc374962f1f884027c4640fd1a7
pjs96328 <pjs96328@gmail.com>
2024-10-14 20:39:06 +0900

    찜 목록 수정

--- src/main/java/com/marketshop/marketshop/service/WishlistResponse.java
+++ src/main/java/com/marketshop/marketshop/service/WishlistResponse.java
@@ -1,16 +1,28 @@
 package com.marketshop.marketshop.service;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class WishlistResponse<T> {
     private String message;
     private T data;
 
-    public WishlistResponse(String message, T data){
+    public WishlistResponse(String message, T data) {
         this.message = message;
+        this.data = data;
+    }
+
+    // Getter, Setter
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
         this.data = data;
     }
 }
