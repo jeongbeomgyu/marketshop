2912974ffc2c498d5fa3d84546464c35fc038e1e
jeongbeomgyu <jeongbeomgyu@github.com>
2024-10-15 22:40:37 +0900

    JWT 토큰 파싱 추가

--- src/main/java/com/marketshop/marketshop/service/MyHandler.java
+++ src/main/java/com/marketshop/marketshop/service/MyHandler.java
@@ -2,6 +2,7 @@ package com.marketshop.marketshop.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.marketshop.marketshop.config.JwtTokenProvider;
 import com.marketshop.marketshop.controller.WebChatController;
 import com.marketshop.marketshop.entity.ChatMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,8 @@ import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.socket.CloseStatus;
@@ -39,6 +42,9 @@ public class MyHandler extends TextWebSocketHandler {
     @Autowired
     SessionManager sessionManager;
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
 
     // 소켓 연결이 완료됐을 때 실행되는 메서드
     @Override
@@ -47,15 +53,65 @@ public class MyHandler extends TextWebSocketHandler {
 
         log.info("연결완료");
 
-        String str = session.getUri().getPath().substring((session.getId().lastIndexOf("/")) + 5);
-        Long roomId = Long.parseLong(str.split("/")[0]);
-        Long userNumber = Long.parseLong(str.split("/")[1]);
+        // WebSocket handshake request에서 Authorization 헤더 추출
+        String token = session.getHandshakeHeaders().getFirst("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7); // "Bearer " 부분을 제거하여 실제 토큰만 남김
+        } else {
+            log.error("JWT 토큰이 존재하지 않음");
+            session.close(CloseStatus.BAD_DATA); // 잘못된 요청으로 소켓을 닫음
+            return;
+        }
 
-        log.info("연결된 소켓의 RoomId는 {}",roomId);
+        // JWT 토큰 검증
+        if (!jwtTokenProvider.validateToken(token)) {
+            log.error("JWT 토큰이 유효하지 않음");
+            session.close(CloseStatus.BAD_DATA); // 잘못된 요청으로 소켓을 닫음
+            return;
+        }
 
+        // 토큰에서 사용자 정보 추출
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String userEmail = authentication.getName();
+        log.info("WebSocket 연결된 사용자: {}", userEmail);
+
+        // URI에서 roomId와 userNumber 추출
+        String path = session.getUri().getPath();
+        String[] segments = path.split("/");
+        if (segments.length < 4) {
+            log.error("잘못된 WebSocket URI: {}", path);
+            session.close(CloseStatus.BAD_DATA);
+            return;
+        }
+
+        Long roomId = Long.parseLong(segments[2]);
+        Long userNumber = Long.parseLong(segments[3]);
+
+        log.info("연결된 소켓의 RoomId는 {}", roomId);
+
+        // 세션 추가
         sessionManager.addSession(roomId, userNumber, session);
 
     }
+
+    // 소켓 연결이 완료됐을 때 실행되는 메서드
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//
+//
+//        log.info("연결완료");
+//
+//        String str = session.getUri().getPath().substring((session.getId().lastIndexOf("/")) + 5);
+//        Long roomId = Long.parseLong(str.split("/")[0]);
+//        Long userNumber = Long.parseLong(str.split("/")[1]);
+//
+//        log.info("연결된 소켓의 RoomId는 {}",roomId);
+//
+//        sessionManager.addSession(roomId, userNumber, session);
+//
+//    }
 
 
     @Override
